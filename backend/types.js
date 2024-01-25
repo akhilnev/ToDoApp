f2a49f6eab77e939f39c1cf7b53362782cebed7e
Akhilesh Nevatia <akhilesh.nevatia@gmail.com>
2024-01-24 20:48:56 -0600

    Adding Zod dependencies

---
+++ backend/types.js
@@ -0,0 +1,12 @@
+const zod = require('zod');
+
+const createToDo = zod.object({
+    title: zod.string(),
+    description: zod.string()
+})
+
+const updateToDo = zod.object({
+    id: zod.string(),
+    completed: zod.boolean()
+})
+
